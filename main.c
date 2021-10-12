2b161d20d3cf461d3189e2925c2c5d7632b4d9ed
Ashish Bhattarai <zeroa5620@gmail.com>
2021-10-12 14:30:52 +0545

    support for muda build

--- main.c
+++ main.c
@@ -5,9 +5,12 @@ int main(int argc, char *argv[]) {
 	csv_parser parser;
 	load_file_buffer(&parser, "./samples/guild-communicators.csv");
 
-	//for (int i = 0; i < 8; i++){
-	//	printf("%s\n", parse_next(&parser));
-	//}
+	for (int col = 0; col < parser.col_count; ++col) {
+		for (int row = 0; row < parser.line_count; ++row) {
+			printf("%s ", parse_next(&parser));
+		}
+		printf("\n");
+	}
 
 	free_csv_resources(&parser);
 	return 0;
